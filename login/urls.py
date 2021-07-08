66d35b383ba02f6737c26aa07f34b3952b9a8ee5
Ryan Yang <ryang1453@gmail.com>
2021-07-07 23:39:08 -0400

    Began with basic url structure

--- login/urls.py
+++ login/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('error/', views.Error.as_view(), name='error'),
+    path('success/', views.Success.as_view(), name='success'),
     path('', views.Index.as_view(), name='index'),
 ]
--- login/views.py
+++ login/views.py
@@ -8,3 +8,19 @@ class Index(View):
 
     def get(self, request: HttpRequest, *args, **kwargs):
         return render(request, self.template_name)
+
+    # TODO: Rate limit post requests to index
+
+
+class Error(View):
+    template_name = 'index.html'
+
+    def get(self, request: HttpRequest, *args, **kwargs):
+        return render(request, self.template_name)
+
+
+class Success(View):
+    template_name = 'index.html'
+
+    def get(self, request: HttpRequest, *args, **kwargs):
+        return render(request, self.template_name)
