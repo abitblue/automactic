069dd92acd41a269208e721d213650a17d317c2d
Ryan Yang <ryang1453@gmail.com>
2021-08-10 10:31:40 -0400

    Self logging

--- deploy_sse.py
+++ deploy_sse.py
@@ -52,10 +52,11 @@ def main():
     hostloggerip = os.environ.get('AMAC_PG_HOST')
     headers = {'user-agent': 'automactic-ping/0.0.1'}
     assert hostloggerip is not None, 'SSE_HOST not defined'
-    httpx.get(f'http://{hostloggerip}/ping', headers=headers, params={
+    req = httpx.get(f'http://{hostloggerip}/ping', headers=headers, params={
         'fromip': get_host_ip(),
         'fromhostname':  socket.gethostname()
     })
+    logger.info(f'Pinged host logger with status code {req.status_code}')
 
     for msg in messages:
         parsed = json.loads(msg.data)
