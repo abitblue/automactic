04fb9d03dac89ffc24eba2676e753104e618a94e
IvanZ505 <zivan7644@gmail.com>
2022-07-28 09:42:43 -0400

    refactor login pages

--- login/views/login.py
+++ login/views/login.py
@@ -4,5 +4,16 @@ from django.http import HttpRequest
 
 
 class Login(View):
-    def get(self, request: HttpRequest, usertype: str):
-        return render(request, f'login/{usertype}.html')
+    def get(self, request: HttpRequest, usertype: str, *args, **kwargs):
+        kiosk: bool = kwargs.pop('kiosk', False)
+        base_dir: str = 'login/batch' if not kiosk else 'login/kiosk'
+
+        help_template = {
+            'student': f"{base_dir}/students.html",
+            'teacher': f"{base_dir}/teachers.html"
+        }
+
+        return render(request, f'login/login.html', {
+            'usertype': usertype.capitalize(),
+            'help_template': help_template[usertype]
+        })
