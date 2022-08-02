ccc39d40541a6972e86e4e81e638d147b0115ed1
abitblue <ryang143@gmail.com>
2022-08-02 13:08:09 -0400

    Deploy: hostapd and firstboot

--- deploy/pi/firstboot.sh
+++ deploy/pi/firstboot.sh
@@ -3,6 +3,15 @@
 # Enable output
 set -x
 
+# Req Vars:
+# HOSTNAME
+# USER_PASSWD
+# VX_VNI
+# VX_GROUP
+# SSID
+# APT_UPGRADE_SKIP
+
+
 # Pull in env vars
 # shellcheck disable=SC2046
 export $(grep -v '^#' /boot/firstboot/config.env | xargs -d '\n')
@@ -36,7 +45,7 @@ timedatectl set-timezone "${TIMEZONE}"
 timedatectl set-ntp true
 
 # Enable ssh, disable root login
-systemctl enable --now ssh
+systemctl enable ssh
 sed -i 's/#\?\(PermitRootLogin\s*\).*$/\1 no/' /etc/ssh/sshd_config
 
 # Enable IP Forwarding
@@ -52,36 +61,47 @@ sed -i -E 's/^(REGDOMAIN[[:blank:]]*=[[:blank:]]*).*/\1us/' /etc/default/crda
 # Unblock the Pi's wlan interface (Wifi Dongle)
 rfkill unblock wlan
 
-# dhcpcd Configuration
-if [ ! -f  /etc/dhcpcd.conf.bak ]; then
-    cp /etc/dhcpcd.conf /etc/dhcpcd.conf.bak
-fi
-cat << EOF >> /etc/dhcpcd.conf
-##################################
-denyinterfaces wlan0 vx-eth0.${VXLAN_ID}
-interface eth0
-    static domain_name_servers=1.1.1.1 8.8.8.8 1.0.0.1 8.8.4.4
-EOF
+# Network configuration - Use systemd netorking
+# Remove non-systemd networking packages
+apt remove --purge --auto-remove dhcpcd5 fake-hwclock ifupdown isc-dhcp-client isc-dhcp-common openresolv
 
-# update apt cache, ingore "Release file... is not valid yet." error
+# Install conf files
+for i in /boot/firstboot/*.{link,netdev,network}; do
+    [ -f "$i" ] || break  # Guard against nonexistant files
+    envsubst < "$i" > "/etc/systemd/network/$(basename "$i")"
+done
+
+# Install hostapd configurations
+cp /boot/firstboot/hostapd@.service /etc/systemd/system
+envsubst < /boot/firstboot/hostapd-ap0.conf > /etc/hostapd/hostapd-ap0.conf
+
+# Enable systemd network services
+systemctl daemon-reload
+systemctl disable wpa_supplicant
+systemctl enable systemd-networkd
+ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
+systemctl enable systemd-resolved
+sytemdctl enable systemd-timesyncd
+systemctl enable hostapd@ap0
+
+# update apt cache and install software, ingore "Release file... is not valid yet." error
 apt update -o Acquire::Check-Valid-Until=false -o Acquire::Check-Date=false
 if [ -z ${APT_UPGRADE_SKIP+x} ]; then apt upgrade -y; else echo "APT_SKIP DEFINED, SKIPPING UPGRADES"; fi
-
-# Install software
-apt install -y dnsutils hostapd vim tmux dos2unix nftables
+apt install -y hostapd vim tmux dos2unix nftables
 
 # Setup nftables
 # TODO: setup nftables
 
-# Setup hostpad
-export LAN_CHANNEL=${LAN_CHANNEL:-"acs_survey"}
-envsubst < /boot/firstboot/hostapd.conf > /etc/hostapd/hostapd.conf
-systemctl unmask hostapd
-
-# Setup vxlan
-systemctl enable systemd-networkd
-
+# Setup automated reboot every day
+systemctl daemon-reload
+cp /boot/firstboot/sched-reboot.service /etc/systemd/system
+cp /boot/firstboot/sched-reboot.timer /etc/systemd/system
+systemctl enable sched-reboot.timer
 
 # Log and finalize
-journalctl -u firstboot.service > /home/pi/firstboot.log
+journalctl -u firstboot.service > /boot/firstboot.log
+
+# Enable overlayfs
+# TODO: Enable overlayfs
+
 systemd-run --no-block sh -c "sleep 15 && systemctl reboot"
