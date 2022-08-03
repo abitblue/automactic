aeef9c0dc9ece92a2ed22ca57e9bd1ce54afe6eb
Anthony Zhen <anthony20151128@gmail.com>
2022-08-03 08:48:54 -0400

    fixed attempt 2

--- interface/api.py
+++ interface/api.py
@@ -1,6 +1,6 @@
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 from wrapper import ResponseData
-from typing import Optional
+from typing import Optional, Union
 import requests
 import logging
 import time
@@ -20,53 +20,60 @@ class Token:
         self.base_url = str(os.environ['BASE_URL'])
         self.api_url = self.base_url + '/oauth'
         self.grant_type = ["client_credentials", "password", "refresh_token"]
-        self.current_time = time.time()
-        self.token_expire_time = self.current_time
+        self.erorr_code = [401, 403]
         self.token = ""
         
+    def renew_token(self):
+        try:
+            req = requests.post(self.api_url, headers={
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            }, json={
+                'grant_type': self.grant_type[0],
+                'client_id': self.id,
+                'client_secret': self.secret,
+            }, verify=False)
+            if req.status_code == 200:
+                token_data = req.json()
+                self.token = token_data["access_token"]
+            elif req.status_code == 400:
+                logging.error(
+                    "Make sure the client id and secret are correct <400(bad req)>")
+      
+        except requests.exceptions.ConnectionError as error:
+            logging.error("Make sure its the right api url", error)
+
+
     def check_token(func):
         def check(*args, **kwargs):
             self = args[0]
-            try:
-                if self.current_time >= self.token_expire_time:
-                    req = requests.post(self.api_url, headers={
-                        'Content-Type': 'application/json',
-                        'Accept': 'application/json',
-                    }, json={
-                        'grant_type': self.grant_type[0],
-                        'client_id': self.id,
-                        'client_secret': self.secret,
-                    }, verify=False)
-                    if req.status_code == 200:
-                        token_data = req.json()
-                        self.token_expire_time = token_data["expires_in"] + self.current_time - 60
-                        self.token = token_data["access_token"]
-                    elif req.status_code == 400:
-                        logging.error(
-                            "Make sure the client id and secret are correct <400(bad req)>")
-                else:
-                    pass
 
-            except requests.exceptions.ConnectionError as error:
-                logging.error("Make sure its the right api url", error)
+            response = func(*args, **kwargs)
 
-            return func(*args, **kwargs)
+            if (response.status_code in self.erorr_code):
+                self.renew_token()
+                response = func(*args, **kwargs)
+
+                if (response.status_code in self.erorr_code):
+                    raise TypeError("ClearPass Token Error")
+
+            return response
 
         return check
 
     @check_token
-    def add_device(self, mac: str, username: str, userType: str, device_name: Optional[str] = None, enabled: bool = True, customTime: timedelta = None) -> dict:
+    def add_device(self, mac: str, username: str, device_name: Optional[str] = None, enabled: bool = True, time: Union[timedelta, datetime] = None) -> dict:
 
         res = requests.post(f"{self.base_url}/device",
                             data=json.dumps({
-                                'expire_time': self._get_expire_date(userType, customTime),
+                                'expire_time': self._get_expire_date(time),
                                 'mac': mac,
                                 'notes': device_name,
                                 'enabled': enabled,
                                 'visitor_name': username,
                                 'role_id': 2,
-                                'do_expire': True,
-                                'start_time': self._get_expire_date('custom', timedelta(minutes=20)) # ClearPass system clock 20 minutes faster
+                                'do_expire': 4,
+                                'start_time': self._get_expire_date(datetime.now() - timedelta(minutes=-20)) # ClearPass system clock 20 minutes faster
                             }),
                             headers=self._get_header(), 
                             verify=False
@@ -105,8 +112,11 @@ class Token:
             'enabled': enabled,
             'role_id': role_id
         }
-        if expire_time: fields['expire_time'] = expire_time
-        if notes: fields['notes'] = notes
+        if expire_time: 
+            fields['expire_time'] = expire_time
+        if notes: 
+            fields['notes'] = notes
+
         if mac is not None:
             res = requests.patch(f"{self.base_url}/device/mac/{mac}",
                 data=json.dumps(fields),
@@ -131,19 +141,14 @@ class Token:
         else:
             raise TypeError('data cannot be empty')
 
-    def _get_expire_date(self, userType: str, custom: timedelta) -> str:
-        now = datetime.now()
+    def _get_expire_date(self, time: Union[timedelta, datetime]) -> str:
         result = None
-        if (userType == 'student'):
-            result = datetime.timestamp(now.replace(month=8, day=31, hour=20, minute=0, second=0) + timedelta(days=4*365))
-        elif (userType == 'teacher'):
-            pass
-        elif (userType == 'guest'):
-            result = datetime.timestamp(now + timedelta(days=1))
-        elif (userType == 'custom'):
-            result = datetime.timestamp(now + custom)
+        if (type(time) == timedelta):
+            result = datetime.timestamp(datetime.now() + time)
+        elif (type(time) == datetime):
+            result = datetime.timestamp(time)
         else:
-            logging.warn(f'{userType} is not a valid User Type, setting expiration to no expiration.\nValid user types are: teacher, student, guest.')
+            logging.error("Invalid time argument!")
 
         return result
 
--- interface/wrapper.py
+++ interface/wrapper.py
@@ -1,8 +1,8 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 @dataclass
 class ResponseData:
-    mac: str = None
+    mac: list[str]
     notes: str = None
     start_time: int = None
     expire_time: int = None
@@ -10,15 +10,22 @@ class ResponseData:
     device_name: str = None
     status_code: int = None
     def __init__(self, status, res):
+        self.mac = []
         try:
             response = res.json()
-            self.mac = response['mac']
+            if (type(response['mac']) != list):
+                if (not self.mac):
+                     self.mac = []
+                self.mac.append(response['mac'])
+            else:
+                self.mac = response['mac']
             self.notes = response['notes']
             self.start_time = response['start_time']
             self.expire_time = response['expire_time']
             self.device_name = response['visitor_name']
         except:
             pass
+
         self.status_code = status
 
     def __eq__(self, other):
