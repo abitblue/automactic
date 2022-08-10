a886d8d46e5c7f3a371fbc6eb472386f7ee73cd9
abitblue <ryang1453@gmail.com>
2022-08-10 00:46:36 -0400

    Revert kiosk handling.

--- login/urls.py
+++ login/urls.py
@@ -4,12 +4,6 @@ from . import views
 
 urlpatterns = [
     path('debug/', views.Debug.as_view(), name='debug'),
-
-    path('kiosk/', include([
-        path('login/<slug:usertype>', views.Login.as_view(), kwargs={'kiosk': True}, name='kiosk_login'),
-        path('', views.Index.as_view(), kwargs={'kiosk': True}, name='kiosk_index'),
-    ])),
-
     path('login/<slug:usertype>', views.Login.as_view(), name='login'),
     path('instructions/', views.Instructions.as_view(), name='instructions'),
     path('success/', views.Success.as_view(), name='success'),
