0c0031a64b50a2168cfda7b11d81139ec6996222
Anthony Zhen <anthony20151128@gmail.com>
2022-08-11 08:03:45 -0400

    fixed typo

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,9 +1,9 @@
-echo "Installing Chromium Browser\n"
+echo "===Installing Chromium Browser==="
 sudo apt install chromium-browser
-echo "Installing Kiosk Service\n"
+echo "===Installing Kiosk Service==="
 sudo cp kiosk.sh /etc/systemd/user
 sudo cp kiosk.service /etc/systemd/user
-echo "Enabling Kiosk Service\n"
+echo "===Enabling Kiosk Service==="
 systemctl --user enable kiosk.service
 systemctl --user start kiosk.service
-echo "Installation Complete\n"
+echo "===Installation Complete==="
--- setup/uninstall.sh
+++ setup/uninstall.sh
@@ -1,5 +1,9 @@
+echo "===Uninstalling Chromium Browser==="
 sudo apt remove chromium-browser
+echo "===Disabling Kiosk Service==="
 systemctl --user disable kiosk.service
 systemctl --user stop kiosk.service
+echo "===Uninstalling Kiosk Service==="
 sudo rm /etc/systemd/user/kiosk.sh 
 sudo rm /etc/systemd/user/kiosk.service
+echo "===Uninstallation Complete==="
