42e938571da1997622bdb4b741c1bf8dedf4f610
Anthony Zhen <anthony20151128@gmail.com>
2022-08-11 08:01:37 -0400

    fixed typo

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,5 +1,9 @@
+echo "Installing Chromium Browser\n"
 sudo apt install chromium-browser
+echo "Installing Kiosk Service\n"
 sudo cp kiosk.sh /etc/systemd/user
 sudo cp kiosk.service /etc/systemd/user
+echo "Enabling Kiosk Service\n"
 systemctl --user enable kiosk.service
 systemctl --user start kiosk.service
+echo "Installation Complete\n"
