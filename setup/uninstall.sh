d1e1e493a83dce766630a6050c9ff85ac6534e2f
AntoDono <anthony20151128@gmail.com>
2022-08-11 00:31:49 -0400

    uninstall.sh

--- setup/uninstall.sh
+++ setup/uninstall.sh
@@ -1,5 +1,5 @@
 sudo apt remove chromium-browser
-cd /etc/systemd/user && sudo systemctl --user disable kiosk.service
-cd /etc/systemd/user && sudo systemctl --user stop kiosk.service
+systemctl --user disable kiosk.service
+systemctl --user stop kiosk.service
 sudo rm /etc/systemd/user/kiosk.sh 
 sudo rm /etc/systemd/user/kiosk.service
