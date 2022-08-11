aa4501ec875ccc97f824c8a3a81232ec95115bc3
Anthony Zhen <anthony20151128@gmail.com>
2022-08-11 09:23:26 -0400

    fixed

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,7 +4,11 @@ echo "===Installing Kiosk Service==="
 sudo cp kiosk.sh /etc/systemd/user
 sudo cp kiosk.service /etc/systemd/user
 sudo chmod +x /etc/systemd/user/kiosk.sh
+sudo cp kioskdjango.service /etc/systemd/user
+sudo chmod +x /etc/systemd/user/kioskdjango.sh
 echo "===Enabling Kiosk Service==="
 systemctl --user enable kiosk.service
 systemctl --user start kiosk.service
+systemctl --user enable kioskdjango.service
+systemctl --user start kioskdjango.service
 echo "===Installation Complete==="
